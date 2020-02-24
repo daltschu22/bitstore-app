7f7daa8f5aacb50537e8c9587fee4ed50b230ad1
daltschu22 <daltschu@broadinstitute.org>
2020-02-24 13:11:40 -0500

    Flask37 (#55)
    * Remove extraneous files, convert files to work with 37
    
    * Changes to upgrade to 37
    
    * Fixes
    
    * Getting farther, working flask
    
    * Lots of progress
    
    * Json broken
    
    * WORKING! dont forget to fix dev stuff
    
    * Working flask and py37
    
    * Fix location of filesystem and class pull
    
    * Learn to post data noob
    
    * linting
    
    * Pull config from firesnore
    
    * Working admin check
    
    * Remove config example
    
    * Dont use cloud function for bigquery queries
    
    * fix historical fs
    
    * fix graph page
    
    * bump version
    
    * testing
    
    * add extended footer
    
    * fix footer
    
    * Render using appengine module
    
    * testing
    
    * switch requirements back and main port
    
    * bump version
    
    * Remove user templates
    
    * Switch over to prod api and prod project
    
    * Ready for merge to master

--- bigquery.py
+++ bigquery.py
@@ -1,25 +1,30 @@
 """Google BigQuery API."""
 
+# import os
+# import datetime
+# import json
 from google.cloud import bigquery
 
 
 class BigQuery():
     """BigQuery class."""
 
-    def __init__(self, project, credentials):
+    def __init__(self, project):
         """Initialize a class instance."""
-        self.bq = bigquery.Client(project=project, credentials=credentials)
+        self.bq = bigquery.Client(project=project)
 
-    def bq_query(self, query):
-        """Query a table with a query argument"""
-        query_job = self.bq.query(query)
+
+    def submit_query(self, query_string):
+        """Query a table with a query argument."""
+        query_job = self.bq.query(query_string)
 
         return query_job.result()
+
 
     def assemble_query_result_list(self, query_result):
         """
         Creates a list of dicts with the keys being the schema field,
-        and the value being the value of that field
+        and the value being the value of that field.
         """
         schema = query_result.schema
         list_of_rows = []
@@ -33,3 +38,12 @@ class BigQuery():
             list_of_rows.append(row_dict)
 
         return list_of_rows
+
+
+    def get_query_results(self, query_string):
+        """Main function, returns query results in list form."""
+        query_result = self.submit_query(query_string)
+        assembled_results = self.assemble_query_result_list(query_result)
+
+        # return json.dumps(assembled_results, indent=2, default=str)
+        return assembled_results
--- develop.sh
+++ develop.sh
@@ -1,39 +1,28 @@
 #!/bin/bash
 
-APP="broad-bitstore-app"
-CONFIG='config-prod.py'
-SERVICEACCOUNT='service_account.pem'
+export PATH=/opt/google-cloud-sdk/bin:$PATH
+
+APP='broad-bitstore-app'
+SERVICEACCOUNT='service_account_prod.json'
 if [ "$1" == 'dev' ]; then
   APP="broad-bitstore-app-dev"
-  CONFIG='config-dev.py'
-  SERVICEACCOUNT='service_account_dev.pem'
+  SERVICEACCOUNT='service_account_dev.json'
+elif [ "$1" == 'sandbox' ]; then
+  APP="broad-bitstore-app-sandbox"
+  SERVICEACCOUNT='service_account_sandbox.json'
 fi
 
-cp ${CONFIG} config.py
+IMAGE="gcr.io/${APP}/bitstore-app:latest"
 
-sudo docker run -it --rm \
-  -v /etc/localtime:/etc/localtime:ro \
-  -v "$(pwd):/usr/src" \
-  -v /local/datastore:/local/datastore:rw \
-  -v "$(pwd)/tmp:/tmp:rw" \
-  --expose 8000 \
+# pull the newest image
+docker pull ${IMAGE}
+
+# run the container
+docker run -it --rm \
   --expose 8080 \
-  -p 8000:8000 \
+  -e GOOGLE_APPLICATION_CREDENTIALS=/usr/src/etc/${SERVICEACCOUNT} \
   -p 8080:8080 \
+  -v `pwd`:/usr/src \
   -w /usr/src \
-  google/cloud-sdk:270.0.0 \
-    dev_appserver.py \
-      -A ${APP} \
-      --admin_host 0.0.0.0 \
-      --admin_port 8000 \
-      --api_port 8081 \
-      --appidentity_email_address=${APP}@appspot.gserviceaccount.com \
-      --appidentity_private_key_path=service_account.pem \
-      --datastore_path /local/datastore/${APP} \
-      --host 0.0.0.0 \
-      --port 8080 \
-      --enable_host_checking false \
-      --skip_sdk_update_check=True \
-      --use_mtime_file_watcher true \
-      app.yaml
-      # --clear_datastore
+  ${IMAGE} \
+  python main.py
